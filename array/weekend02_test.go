ec6fb819fe241ab59abf1f685283a8a2187dee81
longjunxian <longjunxian@sworddata.cn>
2022-02-02 19:54:10 +0800

    数组
    第二周

---
+++ array/weekend02_test.go
@@ -0,0 +1,15 @@
+package main
+
+import "testing"
+
+func TestMoveZeroes(t *testing.T) {
+	nums := []int{0, 1, 0, 3, 12}
+	MoveZeroes(nums)
+	t.Log(nums)
+}
+
+func TestFindKthPositive2(t *testing.T) {
+	nums := []int{2, 3, 4, 7, 11}
+	result := FindKthPositive2(nums, 5)
+	t.Log(result)
+}
