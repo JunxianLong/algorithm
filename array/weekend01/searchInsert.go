95aed423b2cbb6e8c92f892514aaba90b165a900
JunxianLong <2436404363@qq.com>
2022-11-27 22:31:09 +0800

    搜索插入位置

--- array/weekend01/searchInsert.go
+++ array/weekend01/searchInsert.go
@@ -16,3 +16,20 @@ func searchInsert(nums []int, target int) int {
 	}
 	return i
 }
+
+// searchInsert2 搜索插入位置
+func searchInsert2(nums []int, target int) int {
+	i, j := 0, len(nums)-1
+	for i <= j {
+		mid := (i + j) >> 1
+		if nums[mid] == target {
+			return mid
+		} else if nums[mid] < target {
+			i = mid + 1
+		} else {
+			j = mid - 1
+		}
+
+	}
+	return i
+}
